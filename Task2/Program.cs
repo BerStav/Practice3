c3e96435cda5c87f6da389c0a4403bb2b0851747
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-15 22:22:43 +0700

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
+int SumNum( int x)
+    {
+        int NumLeng = Convert.ToString(x).Length;
+
+        int adv = 0;
+        int res = 0;
+
+        for (int i = 0; i < NumLeng; i++)
+            {
+                adv = x - x % 10;
+                res = res + (x - adv);
+                x = x / 10;
+            }
+        return res;
+    }
+
+Console.Write("Please, enter the number: ");
+int numN = int.Parse(Console.ReadLine());
+
+int SumNumber = SumNum(numN);
+Console.WriteLine("The Summ of Number's digits is : " + SumNumber);
